8efe79fdd3dc5df54a95bd3f60a28ab335059c7e
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2024-09-03 20:44:51 -0300

    feat:dashboard logic finish

--- resources/views/tasks/dashboard.blade.php
+++ resources/views/tasks/dashboard.blade.php
@@ -4,6 +4,17 @@
 
 @section('content')
     
+    <div class="list-cards col-10 offset-1">
+        @foreach ($tasks as $task)
+            <div class="card">
+                <div class="card-header text-truncate">{{$task->title}}</div>
+                <div class="card-body">
+                    <h6 class="card-subtitle mb-2 text-muted">{{date('d/m/Y', strtotime($task->date))}}</h6>
 
+                    <p class="card-text text-truncate-6">{{$task->description}}</p>
+                </div>
+            </div>
+        @endforeach
+    </div>
 
 @endsection
