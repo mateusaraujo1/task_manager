1e72ac7587b65146dd45847c415abba6c2955533
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2024-09-03 22:07:46 -0300

    feat: buttons delete and edit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,16 +7,22 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+    {{-- CSS da aplicação --}}
     <link rel="stylesheet" href="/css/index.css">
     <link rel="stylesheet" href="/css/create.css">
     <link rel="stylesheet" href="/css/dashboard.css">
 
+    {{-- bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
+
+    {{-- ícones --}}
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
 </head>
 
--- resources/views/tasks/dashboard.blade.php
+++ resources/views/tasks/dashboard.blade.php
@@ -11,7 +11,12 @@
                 <div class="card-body">
                     <h6 class="card-subtitle mb-2 text-muted">{{date('d/m/Y', strtotime($task->date))}}</h6>
 
-                    <p class="card-text text-truncate-6">{{$task->description}}</p>
+                    <p class="card-text text-truncate-4">{{$task->description}}</p>
+
+                    <div class="card-buttons d-flex justify-content-between position-absolute bottom-0 start-0 end-0 p-3">
+                        <button class="btn btn-info"><ion-icon name="create-outline"></ion-icon></button>
+                        <button class="btn btn-danger"><ion-icon name="trash-outline"></ion-icon></button>
+                    </div>
                 </div>
             </div>
         @endforeach
